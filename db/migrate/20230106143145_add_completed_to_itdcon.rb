f2743deb6153a8d08fe335e712239798c470c6b2
fernando98alba <fjalba@uc.cl>
2023-01-09 14:41:40 -0300

    <Revert "fixing push error"
    This reverts commit f2843dbecb7ca3734b701c7336ee4b40e6a5a325.

--- db/migrate/20230106143145_add_completed_to_itdcon.rb
+++
@@ -1,5 +0,0 @@
-class AddCompletedToItdcon < ActiveRecord::Migration[7.0]
-  def change
-    add_column :itdcons, :completed, :boolean, default: false
-  end
-end
--- db/migrate/20230106143232_add_completed_to_itdind.rb
+++
@@ -1,5 +0,0 @@
-class AddCompletedToItdind < ActiveRecord::Migration[7.0]
-  def change
-    add_column :itdinds, :completed, :boolean,  default: false
-  end
-end
